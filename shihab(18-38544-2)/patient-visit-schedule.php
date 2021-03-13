b86061bb83d01317c0f1686cbe41d38d4ba8f0e3
farialisa99 <farialisa99@gmail.com>
2021-03-14 00:32:18 +0600

    Addded Patient and Visitor

---
+++ shihab(18-38544-2)/patient-visit-schedule.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html >
+<head>
+	<title>
+		
+	</title>
+</head>
+<body>
+<table style="width:80%">
+  <tr>
+    <th>Pateint Serial</th>
+    <th>Patient Name</th>
+    <th>Patient Age</th>
+    <th>Patient Date(D/M/Y)</th>
+    <th>Time</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Sharif</td>
+    <td>50</td>
+    <td>15-03-2021</td>
+    <td>16:30</td>
+
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Ahmed</td>
+    <td>38</td>
+    <td>15-03-2021</td>
+    <td>16:50</td>
+  </tr>
+   <tr>
+    <td>3</td>
+    <td>AAbdullah</td>
+    <td>48</td>
+    <td>16-03-2021</td>
+    <td>18:50</td>
+  </tr>
+   <tr>
+    <td>4</td>
+    <td>Yusuf</td>
+    <td>68</td>
+    <td>15-03-2021</td>
+    <td>19:50</td>
+  </tr>
+   <tr>
+    <td>5</td>
+    <td>Kashem</td>
+    <td>60</td>
+    <td>17-03-2021</td>
+    <td>16:30</td>
+  </tr>
+   <tr>
+    <td>6</td>
+    <td>Riaz Uddin</td>
+    <td>45</td>
+    <td>15-03-2021</td>
+    <td>14:50</td>
+  </tr>
+</table>
+	</body>
+</html>
---
+++ shihab(18-38544-2)/visitor.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>
+		
+	</title>
+</head>
+<body background="222.jpg">
+	
+   
+                    
+                                <label for="Doctor">Category Wise</label>
+        <select id="doctor" name="Doctor">
+            <option value="Dermatologist">Dermatologist</option>
+            <option value="Endrocrinologist">Endrocrinologist</option>
+            <option value="Hematologist">Hematologist</option>
+            <option value="Medicine">Medicine Specialist</option>
+            <option value="Neprologist">Neprologist</option>
+            <option value="Neurologist">Neurologist</option>
+            <option value="Oncologist">Oncologist</option>
+        </select>
+        <br>
+
+  </div>
+</div>
+            </div>
+            <br>
+
+               <ul>
+                    <li ><a href="index.php">See HomePage </a></li>
+                  
+                </ul>
+
+                 <ul>
+                    <li ><a href="doctors-visit-ammount.php">See Doctor's Visit Ammount </a></li>
+                  
+                </ul>
+        </div>
+    </div>
+</div>
+</body>
+</html>
